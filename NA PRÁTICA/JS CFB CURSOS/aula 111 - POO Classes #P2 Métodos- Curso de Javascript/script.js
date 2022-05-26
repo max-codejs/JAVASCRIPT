9cb8d514fc157a8b72e3989a2176e1b0f4f234ee
Max Santos <93297639+max-codejs@users.noreply.github.com>
2022-05-26 00:05:38 -0300

    ok
    ok

--- NA PRÁTICA/JS CFB CURSOS/aula 111 - POO Classes #P2 Métodos- Curso de Javascript/script.js
+++ NA PRÁTICA/JS CFB CURSOS/aula 111 - POO Classes #P2 Métodos- Curso de Javascript/script.js
@@ -11,17 +11,27 @@ class Moto {
         console.log('Marca...........: ' + this.marca)
         console.log('Modelo..........: ' + this.modelo)
         console.log('Cor.............: ' + this.cor)
-        console.log('Ligado..........: ' + (this.ligado ? 'sim' : 'não')) // operador ternário
+        console.log('Ligado..........: ' + (this.ligado ? "Sim" : "Não")) // operador ternário
         console.log('KM..............: ' + this.km)
         console.log('Combustível.....: ' + this.comb)
         console.log('........................ ')
 
-    };
+    }
+    ligar(){
+        this.ligado = true;
+    }
 }
     let m1 = new Moto('Honda','Fan','Azul');
     let m2 = new Moto('Yamaha','Factor','Preta')
     let m3 = new Moto('Dafra','Riva','Branca')
     let m4 = new Moto('Shineray','Phoenix G','Vermelha')
-    
 
+    m4.ligar(); // tem q tá em cima dos outros de baixo pra aparecer o operador ternario 
+    
+    m1.info();
+    m2.info();
+    m3.info();
+    m4.info();
+
+    
     
