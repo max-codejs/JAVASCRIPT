7da2617a0bc222baed0cea3826c3137668c12b05
Max Santos <93297639+max-codejs@users.noreply.github.com>
2022-06-13 22:03:22 -0300

    ok
    ok

--- NA PRÁTICA/JS PROGRAMADOR A BORDO/Origamid-Rest-e-Spread.js
+++ NA PRÁTICA/JS PROGRAMADOR A BORDO/Origamid-Rest-e-Spread.js
@@ -34,6 +34,8 @@ console.log(carro);
 console.log(cloneCarro);
 console.log(carroEsportivo);
 
+// desestruturando objetos
+
 const moto = {
     modelo: 'titan',
     cor: 'vermelha',
@@ -49,3 +51,40 @@ console.log('Fiz uma copia', copia);
 copia.versao = 'KS'; // aqui eu modifico a versao somente da copia
 console.log('Executando', moto); // veja q aqui não modificou nada
 console.log('Veja como fica a copia agora', copia)
+
+// desestruturando arrays e objetos
+
+var usuario = {
+  nome: 'Max',
+  idade: 25,
+  filhos: false,
+  sobrenome: 'santos',
+  profissao: 'empresario'
+}
+
+let {sobrenome, profissao, ...resto} = usuario;
+
+console.log(resto); // veja aqui que todo o restante foi pra variável resto.
+
+var usuario01 = {
+  nome: 'madson',
+  idade: 26,
+  filhos: true,
+  sobrenome: 'santos',
+  profissao: 'mecanico'
+}
+
+var usuario02 = {
+  nome: 'marcelo',
+  idade: 24,
+  filhos: false,
+  sobrenome: 'santos',
+  profissao: 'funcionario'
+}
+
+let alunos = [usuario01, usuario02];
+
+let [madson, marcelo] = alunos; 
+
+console.log('Alunos: ', madson);
+
